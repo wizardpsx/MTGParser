893865e003aa805b98d962cd1c3ab329ffc2a01b
wizardpsx <paulscaccianoce@gmail.com>
2023-05-26 07:28:30 -0700

    fixed issue with errors thrown when deserialzing

--- Program.cs
+++ Program.cs
@@ -30,45 +30,65 @@ try
         using (StreamWriter output = new StreamWriter(Directory.GetCurrentDirectory() + "/output.csv"))
         {
             string legalityLabels = "LEGAL_standard, LEGAL_future, LEGAL_historic, LEGAL_gladiator, LEGAL_pioneer, LEGAL_explorer, LEGAL_modern, LEGAL_legacy, LEGAL_pauper, LEGAL_vintage, LEGAL_penny, LEGAL_commander, LEGAL_oathbreaker, LEGAL_brawl, LEGAL_historicbrawl, LEGAL_alchemy, LEGAL_paupercommander, LEGAL_duel, LEGAL_oldschool, LEGAL_premodern, LEGAL_predh".ToUpper();
-            output.WriteLine($"Name, Set, COLORS, COLOR_IDENTITY, MANA_COST, RARITY, TYPE, TEXT, KEYWORDS, POWER, TOUGHNESS, URL, USD, USD_FOIL, USD_ETCHED, EUR, EUR_FOIL, TIX, {legalityLabels}");
+            output.WriteLine($"Name, SET, COLORS, COLOR_IDENTITY, MANA_COST, RARITY, TYPE, TEXT, KEYWORDS, POWER, TOUGHNESS, URL, USD, USD_FOIL, USD_ETCHED, EUR, EUR_FOIL, TIX, {legalityLabels}");
 
             foreach (var bdata in bulkData)
             {
-                string colors = string.Empty;
-                foreach (string color in bdata.colors)
-                {
-                    colors = colors + color + " ";
-                }
 
-                string coloridentity = string.Empty;
-                foreach (string color in bdata.color_identity)
-                {
-                    coloridentity = coloridentity + color + " ";
-                }
-
-                string keywords = string.Empty;
-                foreach (string key in bdata.keywords)
-                {
-                    keywords = keywords + key + " | ";
-                }
+                string coloridentity = " ";
+                string colors = " ";
+                string keywords = " ";
 
 
-                if (bdata.color_identity.Count() > 0)
+                if (bdata.color_identity != null)
                 {
-                    coloridentity = coloridentity.Substring(0, coloridentity.Length - 1);
+                    if (bdata.color_identity.Count() > 0)
+                    {
+
+                        foreach (string color in bdata.color_identity)
+                        {
+                            coloridentity = coloridentity + color + " ";
+                        }
+                        coloridentity = coloridentity.Substring(0, coloridentity.Length - 1);
+                    }
                 }
-                if (bdata.colors.Count() > 0)
+                if (bdata.colors != null)
                 {
-                    colors = colors.Substring(0, colors.Length - 1);
+                    if (bdata.colors.Count() > 0)
+                    {
+
+                        foreach (string color in bdata.colors)
+                        {
+                            colors = colors + color + " ";
+                        }
+                        colors = colors.Substring(0, colors.Length - 1);
+                    }
                 }
-                if (bdata.keywords.Count() > 0)
+                if (bdata.keywords != null)
                 {
-                    keywords = keywords.Substring(0, keywords.Length - 3);
+                    if (bdata.keywords.Count() > 0)
+                    {
+
+                        foreach (string key in bdata.keywords)
+                        {
+                            keywords = keywords + key + " | ";
+                        }
+                        keywords = keywords.Substring(0, keywords.Length - 3);
+                    }
                 }
 
                 string L = $"{bdata.legalities.standard}, {bdata.legalities.future}, {bdata.legalities.historic}, {bdata.legalities.gladiator}, {bdata.legalities.pioneer}, {bdata.legalities.explorer}, {bdata.legalities.modern}, {bdata.legalities.legacy}, {bdata.legalities.pauper}, {bdata.legalities.vintage}, {bdata.legalities.penny}, {bdata.legalities.commander}, {bdata.legalities.oathbreaker}, {bdata.legalities.brawl}, {bdata.legalities.historicbrawl}, {bdata.legalities.alchemy}, {bdata.legalities.paupercommander}, {bdata.legalities.duel}, {bdata.legalities.oldschool}, {bdata.legalities.premodern}, {bdata.legalities.predh}";
-
-                output.WriteLine($"{bdata.name}, {bdata.setname}, {colors}, {coloridentity}, {bdata.mana_cost}, {bdata.rarity}, {bdata.type_line.Replace(",", " ")}, {bdata.oracle_text.Replace(","," ")}, {keywords}, {bdata.power}, {bdata.toughness}, { bdata.related_uris.gatherer }, {bdata.prices.usd}, {bdata.prices.usd_foil}, {bdata.prices.usd_etched}, {bdata.prices.eur}, {bdata.prices.eur_foil}, {bdata.prices.tix}, {L}");
+                string text = " ";
+                if(!string.IsNullOrEmpty(bdata.oracle_text))
+                {
+                    text = bdata.oracle_text.Replace(","," ").Replace("\n"," ");
+                }
+                string type = string.Empty;
+                if (!string.IsNullOrEmpty(bdata.type_line))
+                {
+                    type = bdata.type_line.Replace(",", " ");
+                }
+                output.WriteLine($"{bdata.name}, {bdata.setname}, {colors}, {coloridentity}, {bdata.mana_cost}, {bdata.rarity}, {type}, {text}, {keywords}, {bdata.power}, {bdata.toughness}, { bdata.related_uris.gatherer }, {bdata.prices.usd}, {bdata.prices.usd_foil}, {bdata.prices.usd_etched}, {bdata.prices.eur}, {bdata.prices.eur_foil}, {bdata.prices.tix}, {L}");
             }
         }
 
