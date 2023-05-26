cc3a15a4165c55fce29da03dd85f90735695f30b
wizardpsx <paulscaccianoce@gmail.com>
2023-05-25 17:11:00 -0700

    Initial commit

---
+++ Models/BulkData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTGParser.Models
+{
+    public class BulkData
+    {
+        public List<Card> Cards { get; set; }
+        public BulkData()
+        {
+            Cards = new List<Card>();
+        }
+    }
+
+    public class Card
+    {
+        public string? name { get; set; }
+        public string? type_line { get; set; }
+        public string? oracle_text { get; set; }
+        public Price prices { get; set; }
+        public RelatedURIS? related_uris { get; set; }
+        public Card()
+        {
+            prices = new Price();
+            related_uris = new RelatedURIS();
+        }
+    }
+
+    public class Price
+    {
+        public string? usd { get; set; }
+        public string? usd_foil { get; set; }
+        public string? usd_etched { get; set; }
+        public string? eur { get; set; }
+        public string? eur_foil { get; set; }
+        public string? tix { get; set; }
+    }
+    public class RelatedURIS
+    {
+        public string gatherer { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MTGParser.Models;
+using System.Text;
+using System.Text.Json;
+
+Console.WriteLine("Download the bulk data json file from https://scryfall.com/docs/api/bulk-data");
+Console.WriteLine("Place file in the same directory as this executable file.");
+Console.WriteLine("Press a key to continue");
+var re = Console.Read();
+
+
+var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.json");
+foreach (var file in files)
+{
+    if(file.Contains("MTGParser."))
+    {
+        continue;
+    }
+
+    if(Path.GetExtension(file) != ".json")
+    {
+        continue;
+    }
+    var data = System.IO.File.ReadAllText(file);
+    Console.WriteLine($"Processing file: {file}");
+
+    var bulkData = JsonSerializer.Deserialize<List<Card>>(data);
+
+
+    using (StreamWriter output = new StreamWriter(Directory.GetCurrentDirectory() + "/output.csv"))
+    {
+        output.WriteLine($"Name, URL, USD, USD_FOIL, USD_ETCHED, EUR, EUR_FOIL, TIX");
+
+        foreach (var bdata in bulkData)
+        {
+            output.WriteLine($"{bdata.name}, { bdata.related_uris.gatherer }, {bdata.prices.usd}, {bdata.prices.usd_foil}, {bdata.prices.usd_etched}, {bdata.prices.eur}, {bdata.prices.eur_foil}, {bdata.prices.tix}");
+        }
+    }
+
+    Console.WriteLine("Finished processing: File \"output.csv\" was created in the same directory as this executable.");
+    Console.ReadLine();
+}
