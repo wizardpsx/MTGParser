6574f18796e82f286b65d1882d61b48d21d82add
wizardpsx <paulscaccianoce@gmail.com>
2023-05-25 19:45:06 -0700

    adding try catch

--- Program.cs
+++ Program.cs
@@ -3,41 +3,49 @@ using MTGParser.Models;
 using System.Text;
 using System.Text.Json;
 
-Console.WriteLine("Download the bulk data json file from https://scryfall.com/docs/api/bulk-data");
-Console.WriteLine("Place file in the same directory as this executable file.");
-Console.WriteLine("Press the enter key to continue");
-var re = Console.ReadLine();
-
-
-var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.json");
-foreach (var file in files)
+try
 {
-    if(file.Contains("MTGParser."))
+    Console.WriteLine("Download the bulk data json file from https://scryfall.com/docs/api/bulk-data");
+    Console.WriteLine("Place file in the same directory as this executable file.");
+    Console.WriteLine("Press the enter key to continue");
+    var re = Console.ReadLine();
+
+
+    var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.json");
+    foreach (var file in files)
     {
-        continue;
-    }
-
-    if(Path.GetExtension(file) != ".json")
-    {
-        continue;
-    }
-    var data = System.IO.File.ReadAllText(file);
-    Console.WriteLine($"Processing file: {file}");
-
-    var bulkData = JsonSerializer.Deserialize<List<Card>>(data);
-
-
-    using (StreamWriter output = new StreamWriter(Directory.GetCurrentDirectory() + "/output.csv"))
-    {
-        output.WriteLine($"Name, URL, USD, USD_FOIL, USD_ETCHED, EUR, EUR_FOIL, TIX");
-
-        foreach (var bdata in bulkData)
+        if (file.Contains("MTGParser."))
         {
-            output.WriteLine($"{bdata.name}, { bdata.related_uris.gatherer }, {bdata.prices.usd}, {bdata.prices.usd_foil}, {bdata.prices.usd_etched}, {bdata.prices.eur}, {bdata.prices.eur_foil}, {bdata.prices.tix}");
+            continue;
         }
-    }
 
-    Console.WriteLine("Finished processing: File \"output.csv\" was created in the same directory as this executable.");
-    Console.WriteLine("Press the enter key to exit");
+        if (Path.GetExtension(file) != ".json")
+        {
+            continue;
+        }
+        var data = System.IO.File.ReadAllText(file);
+        Console.WriteLine($"Processing file: {file}");
+
+        var bulkData = JsonSerializer.Deserialize<List<Card>>(data);
+
+
+        using (StreamWriter output = new StreamWriter(Directory.GetCurrentDirectory() + "/output.csv"))
+        {
+            output.WriteLine($"Name, URL, USD, USD_FOIL, USD_ETCHED, EUR, EUR_FOIL, TIX");
+
+            foreach (var bdata in bulkData)
+            {
+                output.WriteLine($"{bdata.name}, { bdata.related_uris.gatherer }, {bdata.prices.usd}, {bdata.prices.usd_foil}, {bdata.prices.usd_etched}, {bdata.prices.eur}, {bdata.prices.eur_foil}, {bdata.prices.tix}");
+            }
+        }
+
+        Console.WriteLine("Finished processing: File \"output.csv\" was created in the same directory as this executable.");
+        Console.WriteLine("Press the enter key to exit");
+        Console.ReadLine();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
     Console.ReadLine();
 }
